8fc640302dcb9cc9d264f7921ef0416d1ff10086
hharzer <hharzer@live.com>
2022-05-11 07:20:52 +0200

    Github - Wed 11 May 2022 07:20:52 AM CEST

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var ghpages = require('gh-pages');
+const ghpages = require("gh-pages")
 
-ghpages.publish('dist', { branch: 'gh-pages' }, function (err) {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-    } else {
-        console.log('Deployed!');
-        process.exit(0);
-    }
+ghpages.publish("dist", { branch: "gh-pages" }, function (err) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log("Deployed!")
+    process.exit(0)
+  }
 })
